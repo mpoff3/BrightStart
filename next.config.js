19b3ecefd22ea52fe614fa74214432935a36f8c7
mpoff3 <maxpoff12@gmail.com>
2025-02-08 18:29:23 -0500

    Updated our landing page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ['images.unsplash.com'],
+  },
+}
+
+module.exports = nextConfig 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
     unoptimized: true,
   },
 }
